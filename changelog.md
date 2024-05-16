9cfb352ba42e2221c36ea3ae527f501ab72075ec
Meng Li <34143965+Meng6@users.noreply.github.com>
2024-05-16 16:03:25 -0500

    Update to v0.3.2 and add changelog

---
+++ changelog.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## v0.3.2
+
+- Suggested nodes to be updated are based on the `Value Node` provided by [pySHACL](https://github.com/RDFLib/pySHACL)
+- Include paths of the RDF graphs to be validated and SHACL files, as well as the datetime of execution
+- Text report is divided into sections based on the nodes suggested for update to enhance readability
--- rdfvr.py
+++ rdfvr.py
@@ -4,7 +4,7 @@ from pyshacl import validate
 from rdflib import Graph
 import pygraphviz as pgv
 import pandas as pd
-import argparse, json, os
+import textwrap, argparse, json, os
 
 def ensure_dir_exists(file_path):
     dir_path = os.path.split(file_path)[0]
@@ -60,11 +60,11 @@ def extract_errors(results_graph, mappings):
     q = """
         PREFIX : <http://www.w3.org/ns/shacl#>
 
-        SELECT ?focus ?msg ?path
+        SELECT ?focus ?msg ?target
         WHERE {
             ?curr_node :focusNode ?focus .
             ?curr_node :resultMessage ?msg .
-            OPTIONAL { ?curr_node :resultPath ?path. }
+            OPTIONAL { ?curr_node :value ?target}
             OPTIONAL { ?curr_node :detail ?child_node. }
             FILTER (!bound(?child_node))
         }
@@ -72,20 +72,18 @@ def extract_errors(results_graph, mappings):
     rows = []
     for r in results_graph.query(q):
         rows.append(r)
-    errors = pd.DataFrame(rows, columns=["node", "msg", "path"]).replace(mappings, regex=True).drop_duplicates()
+    errors = pd.DataFrame(rows, columns=["node", "msg", "target"]).replace(mappings, regex=True).drop_duplicates()
+    errors["target"] = errors["target"].fillna(errors["node"])
     return errors
 
-def process_graph(rdf_graph, mappings, errors):
+def graph(rdf_graph, mappings):
     """
-    This function is to process a RDF Graph object:
+    This function is to convert a RDF Graph object to a Pandas data frame:
         (1) make the node name shorter
-        (2) export as a Pandas data frame
-        (3) add suggested nodes to be updated to the errors data frame
+        (2) export as a Pandas data frame with source, label, and target columns
     :param rdf_graph: a RDF Graph object
     :param mappings: a dictionary used to make the node name shorter
-    :param errors: a Pandas data frame with node, msg, and path columns
     :return rdf_graph_processed: a Pandas data frame
-    :return errors_with_suggested_nodes: a Pandas data frame with node, msg, and target columns, where target column denotes suggested nodes to be updated
     """
     # Query the imported triples in rdf_graph
     q = """
@@ -99,10 +97,9 @@ def process_graph(rdf_graph, mappings, errors):
     for r in rdf_graph.query(q):
         rows.append(r)
     rdf_graph_processed = pd.DataFrame(rows, columns=["source", "label", "target"]).replace(mappings, regex=True)
-    errors_with_suggested_nodes = errors.merge(rdf_graph_processed, how="left", left_on=["node", "path"], right_on=["source", "label"])[["node", "msg", "target"]]
-    return rdf_graph_processed, errors_with_suggested_nodes
+    return rdf_graph_processed
 
-def visualize_graph_as_dot(rdf_graph_processed, errors_with_suggested_nodes):
+def visualize_graph_as_dot(rdf_graph_processed, errors):
     # Create a directed graph
     G = pgv.AGraph(directed=True)
     # Add nodes and edges
@@ -110,30 +107,40 @@ def visualize_graph_as_dot(rdf_graph_processed, errors_with_suggested_nodes):
         G.add_node(row["source"], shape="box", style="filled, rounded", fillcolor="#b3e2cd")
         G.add_node(row["target"], shape="box", style="filled, rounded", fillcolor="#b3e2cd")
         G.add_edge(row["source"], row["target"], label=row["label"])
-    for _, row in errors_with_suggested_nodes[["node", "msg"]].drop_duplicates().iterrows():
+    for _, row in errors[["node", "msg"]].drop_duplicates().iterrows():
         G.add_node(row["node"], shape="box", style="filled, rounded", fillcolor="#fdccac")
         G.add_node(row["msg"], shape="box", style="filled, rounded, dashed", fillcolor="#fdccac")
         G.add_edge(row["node"], row["msg"], label="ErrorMsg", style="dashed")
     # Suggested nodes to be updated
-    for suggested_node in errors_with_suggested_nodes["target"].dropna().unique():
+    for suggested_node in errors["target"].dropna().unique():
         G.add_node(suggested_node, shape="box", style="filled, rounded", fillcolor="#cbd5e8")
     return G
 
-def report_graph_as_txt(errors_with_suggested_nodes):
+def report_graph_as_txt(errors):
     report_text = ""
-    for _, row in errors_with_suggested_nodes.groupby(["node", "msg"])["target"].agg(list).reset_index().iterrows():
-        report_text = report_text + "Node: {node} \nError Message: {msg}\nSuggested Node(s) to be Updated: {suggested_node}\n\n".format(node=row["node"], msg=row["msg"], suggested_node=str(row["target"]))
+    errors = errors.groupby(["node", "msg"])["target"].agg(list).reset_index()
+    errors["target"] = errors["target"].apply(str)
+    for target, group in errors.groupby("target"):
+        width = 60
+        title = "Suggested Node(s) to be Updated: {target}".format(target=target)
+        wrapped_title = "\n".join(["* " + x.ljust(width - 4) + " *" for x in (textwrap.wrap(title, width - 4))])
+        report_text = report_text + "*" * width + "\n" + wrapped_title + "\n" + "*" * width + "\n".format(target=target)
+        for _, row in group.iterrows():
+            report_text = report_text + "Node: {node} \nError Message: {msg}\n\n".format(node=row["node"], msg=row["msg"])
     return report_text
 
 def validation_report(file_path, file_format, schema_file, schema_format, output_path, output_format, mappings):
+    info = """Path of the RDF graph to be validated: {file_path}
+Path of the SHACL file: {schema_file}
+Datetime: {datetime}
+
+""".format(file_path=file_path, schema_file=schema_file, datetime=pd.Timestamp.now())
     # Load a file with a given format as a RDF Graph object supported by RDFLib
     rdf_graph = load_file(file_path, graph_format=file_format)
     # Validate the RDF graph
     results_graph = validate_rdf(rdf_graph, schema_file, data_graph_format=file_format, shacl_graph_format=schema_format)
     # Find the most important errors
     errors = extract_errors(results_graph, mappings)
-    # Find the suggested error nodes
-    (rdf_graph_processed, errors_with_suggested_nodes) = process_graph(rdf_graph, mappings, errors)
     # Output
     if output_format not in ["txt", "png", "svg", "gv"]:
         raise ValueError("The output file format can only be one of {txt, png, svg, gv}, but " + str(output_format) + " was given. Please check --outputformat.")
@@ -142,13 +149,15 @@ def validation_report(file_path, file_format, schema_file, schema_format, output
         output_path = output_path + "." + output_format
         ensure_dir_exists(output_path)
     if output_format == "png" or output_format == "svg" or output_format == "gv":
-        G = visualize_graph_as_dot(rdf_graph_processed, errors_with_suggested_nodes)
+        print(info)
+        rdf_graph_processed = graph(rdf_graph, mappings)
+        G = visualize_graph_as_dot(rdf_graph_processed, errors)
         if output_format == "png" or output_format == "svg":
             G.draw(output_path, prog="dot")
         else: # gv
             G.write(output_path)
     else: # txt
-        report_text = report_graph_as_txt(errors_with_suggested_nodes)
+        report_text = info + report_graph_as_txt(errors)
         if not output_path:
             # If NO --output, print a string
             print(report_text)
